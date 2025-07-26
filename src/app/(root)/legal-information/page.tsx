ea0d00595fe491bcda6c993eee2045398138fcd2
T-tech26 <taridoutua341@gmail.com>
2025-07-27 00:46:24 +0100

    Suspense hook implemented in news page

--- src/app/(root)/legal-information/page.tsx
+++ src/app/(root)/legal-information/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSearchParams, useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
 const Page = () => {
 
@@ -214,4 +214,62 @@ const Page = () => {
   )
 }
 
-export default Page
+
+const Loading = () => {
+    return (
+        <section className="legal-section">
+            <div className="container">
+
+                <div className="section-header">
+                    <h2>Legal Information</h2>
+                    <p>Important legal documents and policies governing our operations and your interaction with our services</p>
+                </div>
+
+                <div className="legal-content">
+                    <div className="legal-nav">
+                        <button 
+                            className={
+                                `legal-tab text-[.8rem]`
+                            } 
+                        >Privacy Policy
+                        </button>
+
+                        <button 
+                            className={
+                                `legal-tab text-[.8rem]`
+                            }
+                        >Terms of Use
+                        </button>
+
+                        <button 
+                            className={
+                                `legal-tab text-[.8rem]`
+                            } 
+                        >Cookie Policy
+                        </button>
+
+                        <button 
+                            className={
+                                `legal-tab text-[.8rem]`
+                            }
+                        >Refund Policy
+                        </button>
+                    </div>
+
+                    <p className='text-3xl'>Loading...</p>
+                </div>  
+            </div>
+        </section>
+    )
+}
+
+
+const SuspenseWrapper = () => {
+    return (
+        <Suspense fallback={<Loading />}>
+            <Page />
+        </Suspense>
+    )
+}
+
+export default SuspenseWrapper
